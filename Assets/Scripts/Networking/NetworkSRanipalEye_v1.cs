6d04803bfa1f762a8d8653fc1846df9077ddba54
a-baur <alexba.baur@protonmail.com>
2021-09-21 11:41:28 +0200

    SRanipal v1 Support

---
+++ Assets/Scripts/Networking/NetworkSRanipalEye_v1.cs
@@ -0,0 +1,313 @@
+﻿//========= Copyright 2018, HTC Corporation. All rights reserved. ===========
+//========= Modified for networking with Photon
+
+using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+using UnityEngine.Assertions;
+using Photon.Pun;
+using Unity.VisualScripting;
+
+
+namespace ViveSR.anipal.Eye
+{
+    public class NetworkSRanipalEye_v1 : MonoBehaviour
+    {
+        [SerializeField] private Transform[] EyesModels = new Transform[0];
+        [SerializeField] private List<EyeShapeTable> EyeShapeTables;
+        /// <summary>
+        /// Customize this curve to fit the blend shapes of your avatar.
+        /// </summary>
+        [SerializeField] private AnimationCurve EyebrowAnimationCurveUpper;
+        /// <summary>
+        /// Customize this curve to fit the blend shapes of your avatar.
+        /// </summary>
+        [SerializeField] private AnimationCurve EyebrowAnimationCurveLower;
+        /// <summary>
+        /// Customize this curve to fit the blend shapes of your avatar.
+        /// </summary>
+        [SerializeField] private AnimationCurve EyebrowAnimationCurveHorizontal;
+
+        public bool NeededToGetData = true;
+        private Dictionary<EyeShape, float> EyeWeightings = new Dictionary<EyeShape, float>();
+        private AnimationCurve[] EyebrowAnimationCurves = new AnimationCurve[(int)EyeShape.Max];
+        private GameObject[] EyeAnchors;
+        private const int NUM_OF_EYES = 2;
+        private static EyeData eyeData = new EyeData();
+        private bool eye_callback_registered = false;
+
+        private PhotonView _photonView;
+
+        private void Start()
+        {
+            _photonView = GetComponent<PhotonView>();
+            /**if (!SRanipal_Eye_Framework.Instance.EnableEye)
+            {
+                Debug.LogError("[SRanipal] Eye disabled!");
+                enabled = false;
+                return;
+            }*/
+
+            SetEyesModels(EyesModels[0], EyesModels[1]);
+            SetEyeShapeTables(EyeShapeTables);
+
+            AnimationCurve[] curves = new AnimationCurve[(int)EyeShape.Max];
+            for (int i = 0; i < EyebrowAnimationCurves.Length; ++i)
+            {
+                if (i == (int)EyeShape.Eye_Left_Up || i == (int)EyeShape.Eye_Right_Up) curves[i] = EyebrowAnimationCurveUpper;
+                else if (i == (int)EyeShape.Eye_Left_Down || i == (int)EyeShape.Eye_Right_Down) curves[i] = EyebrowAnimationCurveLower;
+                else curves[i] = EyebrowAnimationCurveHorizontal;
+            }
+            SetEyeShapeAnimationCurves(curves);
+        }
+
+        private void Update()
+        {
+            if (SRanipal_Eye_Framework.Status != SRanipal_Eye_Framework.FrameworkStatus.WORKING &&
+                SRanipal_Eye_Framework.Status != SRanipal_Eye_Framework.FrameworkStatus.NOT_SUPPORT) return;
+
+            if (!NeededToGetData | !_photonView.IsMine) return;
+
+            if (SRanipal_Eye_Framework.Instance.EnableEyeDataCallback == true && eye_callback_registered == false)
+            {
+                SRanipal_Eye.WrapperRegisterEyeDataCallback(Marshal.GetFunctionPointerForDelegate((SRanipal_Eye.CallbackBasic)EyeCallback));
+                eye_callback_registered = true;
+            }
+            else if (SRanipal_Eye_Framework.Instance.EnableEyeDataCallback == false && eye_callback_registered == true)
+            {
+                SRanipal_Eye.WrapperUnRegisterEyeDataCallback(Marshal.GetFunctionPointerForDelegate((SRanipal_Eye.CallbackBasic)EyeCallback));
+                eye_callback_registered = false;
+            }
+            else if (SRanipal_Eye_Framework.Instance.EnableEyeDataCallback == false)
+                SRanipal_Eye_API.GetEyeData(ref eyeData);
+
+            bool isLeftEyeActive = false;
+            bool isRightEyeAcitve = false;
+            if (SRanipal_Eye_Framework.Status == SRanipal_Eye_Framework.FrameworkStatus.WORKING)
+            {
+                isLeftEyeActive = eyeData.no_user;
+                isRightEyeAcitve = eyeData.no_user;
+            }
+            else if (SRanipal_Eye_Framework.Status == SRanipal_Eye_Framework.FrameworkStatus.NOT_SUPPORT)
+            {
+                isLeftEyeActive = true;
+                isRightEyeAcitve = true;
+            }
+
+            if (isLeftEyeActive || isRightEyeAcitve)
+            {
+                if (eye_callback_registered == true)
+                    SRanipal_Eye.GetEyeWeightings(out EyeWeightings, eyeData);
+                else
+                    SRanipal_Eye.GetEyeWeightings(out EyeWeightings);
+
+                Dictionary<byte, float> binEyeWeightings = BinaryEyeWeightings(EyeWeightings);
+                _photonView.RPC("UpdateEyeShapes", RpcTarget.All, binEyeWeightings);
+            }
+            else
+            {
+                for (int i = 0; i < (int)EyeShape.Max; ++i)
+                {
+                    bool isBlink = ((EyeShape)i == EyeShape.Eye_Left_Blink || (EyeShape)i == EyeShape.Eye_Right_Blink);
+                    EyeWeightings[(EyeShape)i] = isBlink ? 1 : 0;
+                }
+
+                Dictionary<byte, float> binEyeWeightings = BinaryEyeWeightings(EyeWeightings);
+                _photonView.RPC("UpdateEyeShapes", RpcTarget.All, binEyeWeightings);
+                return;
+            }
+
+            Vector3 GazeOriginCombinedLocal, GazeDirectionCombinedLocal = Vector3.zero;
+            if (eye_callback_registered == true)
+            {
+                if (SRanipal_Eye.GetGazeRay(GazeIndex.COMBINE, out GazeOriginCombinedLocal, out GazeDirectionCombinedLocal, eyeData)) { }
+                else if (SRanipal_Eye.GetGazeRay(GazeIndex.LEFT, out GazeOriginCombinedLocal, out GazeDirectionCombinedLocal, eyeData)) { }
+                else if (SRanipal_Eye.GetGazeRay(GazeIndex.RIGHT, out GazeOriginCombinedLocal, out GazeDirectionCombinedLocal, eyeData)) { }
+            }
+            else
+            {
+                if (SRanipal_Eye.GetGazeRay(GazeIndex.COMBINE, out GazeOriginCombinedLocal, out GazeDirectionCombinedLocal)) { }
+                else if (SRanipal_Eye.GetGazeRay(GazeIndex.LEFT, out GazeOriginCombinedLocal, out GazeDirectionCombinedLocal)) { }
+                else if (SRanipal_Eye.GetGazeRay(GazeIndex.RIGHT, out GazeOriginCombinedLocal, out GazeDirectionCombinedLocal)) { }
+
+            }
+
+            if (_photonView.IsMine)
+            {
+                _photonView.RPC("UpdateGazeRay", RpcTarget.All, GazeDirectionCombinedLocal);
+            }
+        }
+        private void Release()
+        {
+            if (eye_callback_registered == true)
+            {
+                SRanipal_Eye.WrapperUnRegisterEyeDataCallback(Marshal.GetFunctionPointerForDelegate((SRanipal_Eye.CallbackBasic)EyeCallback));
+                eye_callback_registered = false;
+            }
+        }
+        private void OnDestroy()
+        {
+            DestroyEyeAnchors();
+        }
+
+        public void SetEyesModels(Transform leftEye, Transform rightEye)
+        {
+            if (leftEye != null && rightEye != null)
+            {
+                EyesModels = new Transform[NUM_OF_EYES] { leftEye, rightEye };
+                DestroyEyeAnchors();
+                CreateEyeAnchors();
+            }
+        }
+
+        // Initilize EyeShape-Blendshape mapping
+        public void SetEyeShapeTables(List<EyeShapeTable> eyeShapeTables)
+        {
+            bool valid = true;
+            if (eyeShapeTables == null)
+            {
+                valid = false;
+            }
+            else
+            {
+                for (int table = 0; table < eyeShapeTables.Count; ++table)
+                {
+                    SkinnedMeshRenderer tableSMR = eyeShapeTables[table].skinnedMeshRenderer;
+                    if (eyeShapeTables[table].skinnedMeshRenderer == null)
+                    {
+                        valid = false;
+                        break;
+                    }
+                    for (int shape = 0; shape < eyeShapeTables[table].eyeShapes.Length; ++shape)
+                    {
+                        EyeShape eyeShape = eyeShapeTables[table].eyeShapes[shape];
+                        if (eyeShape > EyeShape.Max || eyeShape < 0)
+                        {
+                            string blendshape = tableSMR.sharedMesh.GetBlendShapeName(shape);
+                            Debug.LogWarning($"[SRanipal] Blendshape '{blendshape}' assigned to invalid EyeShape '{eyeShape}'. Facial animation may not be accurate!");
+                            continue;
+                        }
+                    }
+                }
+            }
+            if (valid)
+            {
+                EyeShapeTables = eyeShapeTables;
+            }
+        }
+
+        public void SetEyeShapeAnimationCurves(AnimationCurve[] eyebrowAnimationCurves)
+        {
+            if (eyebrowAnimationCurves.Length == (int)EyeShape.Max)
+                EyebrowAnimationCurves = eyebrowAnimationCurves;
+        }
+
+        // Set direction of gaze.
+        // Call on remote.
+        [PunRPC]
+        public void UpdateGazeRay(Vector3 gazeDirectionCombinedLocal)
+        {
+            for (int i = 0; i < EyesModels.Length; ++i)
+            {
+                Vector3 target = EyeAnchors[i].transform.TransformPoint(gazeDirectionCombinedLocal);
+                EyesModels[i].LookAt(target);
+            }
+        }
+
+
+        // Update all blendshapes models in EyeShapeTables.
+        // Call on remote.
+        [PunRPC]
+        public void UpdateEyeShapes(Dictionary<byte, float> binEyeWeightings)
+        {
+            Dictionary<EyeShape, float> eyeWeightings = EnumEyeWeightings(binEyeWeightings);
+
+            foreach (var table in EyeShapeTables)
+                RenderModelEyeShape(table, eyeWeightings);
+        }
+
+        // Apply Eye Tracker weightings onto blendshape model
+        private void RenderModelEyeShape(EyeShapeTable eyeShapeTable, Dictionary<EyeShape, float> weighting)
+        {
+            for (int i = 0; i < eyeShapeTable.eyeShapes.Length; ++i)
+            {
+                EyeShape eyeShape = eyeShapeTable.eyeShapes[i];
+                if (eyeShape > EyeShape.Max || eyeShape < 0) continue;
+
+                if (eyeShape == EyeShape.Eye_Left_Blink || eyeShape == EyeShape.Eye_Right_Blink)
+                    eyeShapeTable.skinnedMeshRenderer.SetBlendShapeWeight(i, weighting[eyeShape] * 100f);
+                else
+                {
+                    AnimationCurve curve = EyebrowAnimationCurves[(int)eyeShape];
+                    eyeShapeTable.skinnedMeshRenderer.SetBlendShapeWeight(i, curve.Evaluate(weighting[eyeShape]) * 100f);
+                }
+            }
+        }
+
+        private void CreateEyeAnchors()
+        {
+            EyeAnchors = new GameObject[NUM_OF_EYES];
+            for (int i = 0; i < NUM_OF_EYES; ++i)
+            {
+                EyeAnchors[i] = new GameObject();
+                EyeAnchors[i].name = "EyeAnchor_" + i;
+                EyeAnchors[i].transform.SetParent(gameObject.transform);
+                EyeAnchors[i].transform.localPosition = EyesModels[i].localPosition;
+                EyeAnchors[i].transform.localRotation = EyesModels[i].localRotation;
+                EyeAnchors[i].transform.localScale = EyesModels[i].localScale;
+            }
+        }
+
+        private void DestroyEyeAnchors()
+        {
+            if (EyeAnchors != null)
+            {
+                foreach (var obj in EyeAnchors)
+                    if (obj != null) Destroy(obj);
+            }
+        }
+        private static void EyeCallback(ref EyeData eye_data)
+        {
+            eyeData = eye_data;
+        }
+
+        /// <summary>
+        /// Converts EyeWeightings to be serializable by Photon.
+        /// </summary> 
+        /// <param name="eyeWeightings">EyeWeightings to be converted.</param>
+        /// <returns>
+        /// Serializable Dictionary of EyeWeightings with byte keys.
+        /// </returns>
+        private Dictionary<byte, float> BinaryEyeWeightings(Dictionary<EyeShape, float> eyeWeightings)
+        {
+            Dictionary<byte, float> binEyeWeightings = new Dictionary<byte, float>();
+
+            foreach (KeyValuePair<EyeShape, float> eyeWeight in eyeWeightings)
+            {
+                binEyeWeightings[(byte)eyeWeight.Key] = eyeWeight.Value;
+            }
+
+            return binEyeWeightings;
+        }
+
+        /// <summary>
+        /// Converts EyeWeightings from binary to enumerated.
+        /// </summary>
+        /// <param name="eyeWeightings">EyeWeightings to be converted.</param>
+        /// <returns>
+        /// Dictionary with enumeration of EyeShapes.
+        /// </returns>
+        private Dictionary<EyeShape, float> EnumEyeWeightings(Dictionary<byte, float> binEyeWeightings)
+        {
+            Dictionary<EyeShape, float> eyeWeightings = new Dictionary<EyeShape, float>();
+
+            foreach (KeyValuePair<byte, float> eyeWeight in binEyeWeightings)
+            {
+                eyeWeightings[(EyeShape)eyeWeight.Key] = eyeWeight.Value;
+            }
+
+            return eyeWeightings;
+        }
+    }
+}
---
+++ Assets/Scripts/Networking/NetworkSRanipalLip_v1.cs
@@ -0,0 +1,143 @@
+//========= Copyright 2019, HTC Corporation. All rights reserved. ===========
+//========= Modified for networking with Photon
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace ViveSR.anipal.Lip
+{
+    public class NetworkSRanipalLip_v1 : MonoBehaviour
+    {
+        [SerializeField] private List<LipShapeTable> LipShapeTables;
+
+        public bool NeededToGetData = true;
+        private Dictionary<LipShape, float> LipWeightings;
+
+        private PhotonView _photonView;
+
+
+        private void Start()
+        {
+
+            if (!SRanipal_Lip_Framework.Instance.EnableLip)
+            {
+                Debug.LogError("[SRanipal] Lip disabled!");
+                enabled = false;
+                return;
+            }
+
+            SetLipShapeTables(LipShapeTables);
+        }
+
+        private void Update()
+        {
+            if (!SRanipal_Lip_Framework.Instance.EnableLip)
+            {
+                enabled = false;
+                return;
+            }
+
+            if (NeededToGetData && _photonView.IsMine)
+            {
+                SRanipal_Lip.GetLipWeightings(out LipWeightings);
+                Dictionary<byte, float> binLipWeightings = BinaryLipWeightings(LipWeightings);
+                        
+                // Update Blendshapes for all players
+                _photonView.RPC("UpdateLipShapes", RpcTarget.All, binLipWeightings);
+                        
+            }
+        }
+        
+        // Initilize Blendshape-Lipshape mapping
+        public void SetLipShapeTables(List<LipShapeTable> lipShapeTables)
+        {
+            bool valid = true;
+            if (lipShapeTables == null)
+            {
+                valid = false;
+                Debug.LogError("[SRanipal] No blendshape mappings defined. Must define Lip Shape Table.");
+            }
+            else
+            {
+                for (int table = 0; table < lipShapeTables.Count; ++table)
+                {
+                    // Check that LipShapeTable has corresponding SkinnedMeshRenderer
+                    SkinnedMeshRenderer tableSMR = lipShapeTables[table].skinnedMeshRenderer;
+                    if (tableSMR == null)
+                    {
+                        valid = false;
+                        Debug.LogError($"[SRanipal] SkinnedMeshRenderer in Lip Shape Table {table} not found.");
+                        break;
+                    }
+                    for (int shape = 0; shape < lipShapeTables[table].lipShapes.Length; ++shape)
+                    {
+                        // Check that LipShape is within range of possible LipShapes
+                        // Can only really fail, if version 1 and 2 are mixed up?
+                        LipShape lipShape = lipShapeTables[table].lipShapes[shape];
+                        if (lipShape > LipShape.Max || lipShape < 0)
+                        {
+                            string blendshape = tableSMR.sharedMesh.GetBlendShapeName(shape);
+                            Debug.LogWarning($"[SRanipal] Blendshape '{blendshape}' assigned to invalid LipShape '{lipShape}'. Facial animation may not be accurate!");
+                            continue;
+                        }
+                    }
+                }
+            }
+            if (valid)
+            {
+                LipShapeTables = lipShapeTables;
+            }
+        }
+
+        // Update all blendshapes models in LipShapeTables.
+        // Call on remote.
+        [PunRPC]
+        public void UpdateLipShapes(Dictionary<byte, float> binLipWeightings)
+        {
+            Dictionary<LipShape, float> lipWeightings = EnumLipWeightings(binLipWeightings);
+                    
+            foreach (var table in LipShapeTables)
+                RenderModelLipShape(table, lipWeightings);
+        }
+
+        // Apply Facial Tracker weightings onto blendshape model
+        private void RenderModelLipShape(LipShapeTable lipShapeTable, Dictionary<LipShape, float> weighting)
+        {
+            for (int i = 0; i < lipShapeTable.lipShapes.Length; i++)
+            {
+                int targetIndex = (int)lipShapeTable.lipShapes[i];
+                if (targetIndex > (int)LipShape.Max || targetIndex < 0) continue;
+                lipShapeTable.skinnedMeshRenderer.SetBlendShapeWeight(i, weighting[(LipShape)targetIndex] * 100);
+            }
+        }
+                
+        /// Converts LipWeightings from enumerated to binary.
+        /// Enables serialization for Photon.
+        private Dictionary<byte, float> BinaryLipWeightings(Dictionary<LipShape, float> lipWeightings)
+        {
+            Dictionary<byte, float> binLipWeightings = new Dictionary<byte, float>();
+                    
+            foreach (KeyValuePair<LipShape, float> lipWeight in lipWeightings)
+            {
+                binLipWeightings[(byte) lipWeight.Key] = lipWeight.Value;
+            }
+
+            return binLipWeightings;
+        }
+                
+        // Converts LipWeightings from binary to enumerated.
+        private Dictionary<LipShape, float> EnumLipWeightings(Dictionary<byte, float> binLipWeightings)
+        {
+            Dictionary<LipShape, float> lipWeightings = new Dictionary<LipShape, float>();
+                    
+            foreach (KeyValuePair<byte, float> lipWeight in binLipWeightings)
+            {
+                lipWeightings[(LipShape) lipWeight.Key] = lipWeight.Value;
+            }
+
+            return lipWeightings;
+        }
+    }
+}
